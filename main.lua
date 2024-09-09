acdb70cc6759524322b32f61908017b8d4b40d77
DOORSCHEATS <bcgperformance.contact@gmail.com>
2024-09-09 04:08:35 +0200

    77

--- main.lua
+++ main.lua
@@ -1,18 +1,48 @@
 local AllowedGames = {
     [1] = 10549820578
- }
+}
  
- local NotAllowedGames = {
+local NotAllowedGames = {
     [1] = 6516141723,
     [2] = 12308344607
- }
- 
- if AllowedGames[table.find(AllowedGames, game.PlaceId)] then
-     print("Allowed")
- else
-     if NotAllowedGames[game.PlaceId] then
-         print("Not allowed")
-     else
-         print("cant check valid id")    
-     end 
+}
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+
+local function Notify(NotifName, NotifContent, NotifImage, NotifTime)
+    OrionLib:MakeNotification({
+        Name = NotifName or "Title!",
+        Content = NotifContent or "Notification content... what will it say??",
+        Image = NotifImage or "rbxassetid://4483345998",
+        Time = NotifTime or 5
+    })
+end
+
+if AllowedGames[table.find(AllowedGames, game.PlaceId)] then
+    print("Allowed")
+    Notify(
+        "WELCOME!",
+        "Thanks for using MDOORS, join our discord () for more updates and infos.",
+        nil,
+        4
+    )
+else
+    if NotAllowedGames[game.PlaceId] then
+        print("Not allowed")
+        Notify(
+            "Error",
+            "This Place/Floor isn't supported. Try to run the script in an another floor.",
+            nil,
+            6
+        )
+    else
+        print("cant check valid id")    
+        Notify(
+            "Error",
+            "Failed to check id, retry again. If it still fail, contact us on our discord ().",
+            nil,
+            5
+        )
+    end 
  end
