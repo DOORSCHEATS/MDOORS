ba2791532a720e9b152651180c015e882399fa26
DOORSCHEATS <bcgperformance.contact@gmail.com>
2024-09-09 02:51:35 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,10 +2,24 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local SoundService = game:GetService("SoundService")
 
-local Utils = {
-    
+local AllowedGames = {
+    [1] = 6516141723  
 }
 
-if game.PlaceId == 6516141723 then
-    print("Right place")
-end 
+function CheckForGameId(Id)
+    if AllowedGames[table.find(AllowedGames, Id)] then
+        return AllowedGames[table.find(AllowedGames, Id)], true
+    else
+        return 0, false
+    end
+end
+
+task.defer(function()
+    local Id, CanStart = CheckForGameId(game.PlaceId)
+
+    if CanStart then
+        print(Id)
+    else
+        print("PLACE "..Id.." is not supported yet.") 
+    end
+end)
