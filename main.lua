6c5ed3809456fa6ef914a9dd9393e3536d4ae17c
DOORSCHEATS <bcgperformance.contact@gmail.com>
2024-09-09 02:00:00 +0200

    first commit

---
+++ main.lua
@@ -0,0 +1,21 @@
+--[[
+    © 2024 MDOORS. All rights reserved. This application, named MDOORS, was created on September 9th, 2024.
+
+    Unauthorized reproduction or distribution of this application, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law.
+
+    For permissions or inquiries, please contact us at crackgamescontact@gmail.com
+
+    Thank you for supporting our hard work and respecting our intellectual property rights!
+]]--
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local SoundService = game:GetService("SoundService")
+
+local _Utils = {
+    GetPlaceId = require(ReplicatedFirst:FindFirstChild("GetPlaceId"))
+}
+
+if game.PlaceId == 6516141723 then
+    print("nigga")
+end
